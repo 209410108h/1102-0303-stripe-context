05a8bb0d556d9ab55813ac3333c1aad3a38199e3
209410108h <hank5947887@gmail.com>
2022-03-03 21:12:09 +0800

    first commit

---
+++ src/App_08.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar_08 from './Navbar_08'
+import Hero_08 from './Hero_08'
+import Sidebar_08 from './Sidebar_08'
+import Submenu_08 from './Submenu_08'
+function App_08() {
+  return (
+    <>
+      <Navbar_08 />
+      <Sidebar_08 />
+      <Hero_08 />
+      <Submenu_08 />
+    </>
+  )
+}
+
+export default App_08
---
+++ src/Submenu_08.js
@@ -0,0 +1,7 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+const Submenu_08 = () => {
+  return <h2>submenu component</h2>
+}
+
+export default Submenu_08
